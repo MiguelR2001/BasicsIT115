1e402f977205009f325c6753e3393e00fa6d7f78
Miguel Rodriguez <rrmiguel2001@gmail.com>
2023-05-17 11:30:56 -0700

    adds button and comments

--- tkinter_intro.py
+++ tkinter_intro.py
@@ -1,2 +1,19 @@
-#
+#Importing the tkinter library.
 import tkinter as tk
+
+
+#Creates function for when button is clicked.
+def button_click():
+    print("Button Clicked!")
+
+
+#Creates parent window or root window.
+root = tk.Tk()
+root.title("Button Example")
+
+#Creates button with 3 parameters; class library, text, function call.
+button = tk.Button(root, text="Click Me!", command=button_click)
+button.pack()
+
+#Creates mainloop to keep the root window open.
+root.mainloop()
